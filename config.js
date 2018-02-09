e999e914013cdd3ce53bb2d059513d77c3f2cad1
ViewUse <v_cjc@caojinchengdeMac-mini.local>
2018-02-09 14:24:28 +0800

    config

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
-var GH_SEARCH_REPO_URL = "https://api.github.com/search/repositories?";
-var GH_SEARCH_USER_URL = "https://api.github.com/search/users?";
-var GH_SEARCH_CODE_URL = "https://api.github.com/search/code?";
+//var GH_SEARCH_REPO_URL = "https://api.unclay.com/cache?url=https://api.github.com/search/repositories&";
+//var GH_SEARCH_USER_URL = "https://api.unclay.com/cache?url=https://api.github.com/search/users&";
+//var GH_SEARCH_CODE_URL = "https://api.unclay.com/cache?url=https://api.github.com/search/code&";
+var GH_SEARCH_REPO_URL = "https%3A%2F%2Fapi.github.com%2Fsearch%2Frepositories&";
+var GH_SEARCH_USER_URL = "https%3A%2F%2Fapi.github.com%2Fsearch%2Fusers&";
+var GH_SEARCH_CODE_URL = "https%3A%2F%2Fapi.github.com%2Fsearch%2Fcode&";
 
 
 module.exports = {
+	SERVER_URL : "https://api.unclay.com/cache?url=",
 	REPO_URL : GH_SEARCH_REPO_URL,
 	USER_URL : GH_SEARCH_USER_URL,
 	CODE_URL : GH_SEARCH_CODE_URL
